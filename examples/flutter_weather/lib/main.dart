f6851d9a450fe48acf47235b3f92617a3a028ca2
Edryn <edryn.magcalas@gmail.com>
2023-09-22 19:35:51 +0800

    barrels and refactor

--- examples/flutter_weather/lib/main.dart
+++ examples/flutter_weather/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_weather/app.dart';
-import 'package:flutter_weather/weather_bloc_observer.dart';
+import 'package:flutter_weather/app/app.dart';
+import 'package:flutter_weather/app/weather_bloc_observer.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:weather_repository/weather_repository.dart';
--- examples/flutter_weather/test/app_test.dart
+++ examples/flutter_weather/test/app_test.dart
@@ -2,7 +2,7 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_weather/app.dart';
+import 'package:flutter_weather/app/app.dart';
 import 'package:flutter_weather/theme/theme.dart';
 import 'package:flutter_weather/weather/weather.dart';
 import 'package:mocktail/mocktail.dart';
